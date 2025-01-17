14285d874b8dd4cea9ec6e22d60bea525ad6fde0
IsItLucas? <lucas.galonet.com@gmail.com>
2025-01-16 23:59:31 -0300

    Update settings path and update process mode.
    Updated settings path for additional compatibility with future "Godot Easy" addons.
    
    Also updated "process_mode" in "autoload.gd" to make sure it can run even when the tree is paused.

--- addons/easy_fullscreen/plugin.gd
+++ addons/easy_fullscreen/plugin.gd
@@ -5,12 +5,12 @@ extends EditorPlugin
 func _enter_tree() -> void:
 	add_autoload_singleton("Fullscreen", "res://addons/easy_fullscreen/autoload.gd")
 	
-	ProjectSettings.set_setting("easy_fullscreen/enabled", true)
-	ProjectSettings.set_setting("easy_fullscreen/action", "fullscreen")
+	ProjectSettings.set_setting("godot_easy/fullscreen/enabled", true)
+	ProjectSettings.set_setting("godot_easy/fullscreen/action", "fullscreen")
 
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("Fullscreen")
 	
-	ProjectSettings.set_setting("easy_fullscreen/enabled", null)
-	ProjectSettings.set_setting("easy_fullscreen/action", null)
+	ProjectSettings.set_setting("godot_easy/fullscreen/enabled", null)
+	ProjectSettings.set_setting("godot_easy/fullscreen/action", null)
